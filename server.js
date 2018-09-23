420802999fb403fcc60512d932f549ef2fda2447
William To <towilliam03@gmail.com>
2018-09-23 01:45:58 -0400

    update logs

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ app.post('/testdistance', (req, res) => {
     let destinationObjs = [];
     let places = [];
     console.log('userAddress: ' + userAddress);
+    console.log('req.body.distanceFromLocation: ' + req.body.distanceFromLocation);
     console.log('userDistance: ' + userDistance);
 
     // Get server data for list of postings
