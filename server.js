bf29712103dccd7d94bd91d4cbb5726f332e9ba9
William To <towilliam03@gmail.com>
2018-09-23 01:51:17 -0400

    add body parser

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const serverData = dataService(connectionString);
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.bodyParser());
 
 app.get('/customers', (req, res) => {
     serverData.getAllCustomers().then((data) => {
